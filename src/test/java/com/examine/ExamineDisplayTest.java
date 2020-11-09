91462d41b11955ea7144ddf56da3ba78d09c17b7
Nathan Moore <nlm105@francis.edu>
2020-11-09 15:59:09 -0500

    Added JUnit tests

--- src/test/java/com/examine/ExamineDisplayTest.java
+++ src/test/java/com/examine/ExamineDisplayTest.java
@@ -6,25 +6,44 @@ import org.junit.jupiter.api.*;
 
 
 public class ExamineDisplayTest {
-//    @Inject
-//    ExaminePanel examinePanel = new ExaminePanel();
-    private static ExaminePanel examinePanel;
-
-    @BeforeAll
-    public static void startUp(){
-        examinePanel = new ExaminePanel();
+    @Test
+    @DisplayName("Link output of NPC name")
+    void testNPCLink() {
+        Assertions.assertEquals("<a href=\"https://oldschool.runescape.wiki/w/Goblin\">Goblin</a>",
+                ExaminePanel.getWikiLink("Goblin"));
     }
 
     @Test
-    @DisplayName("Test to make sure panels are Labels are initially not visible")
-    public void testVisible(){
-        Assertions.assertAll(() -> Assertions.assertFalse(ExaminePanel.type.isVisible()),
-                () -> Assertions.assertFalse(ExaminePanel.name.isVisible()),
-                () -> Assertions.assertFalse(ExaminePanel.wikiLnk.isVisible()));
+    @DisplayName("Link output of Item name")
+    void testItemLink() {
+        Assertions.assertEquals("<a href=\"https://oldschool.runescape.wiki/w/Knife\">Knife</a>",
+                ExaminePanel.getWikiLink("Knife"));
     }
 
     @Test
-    void testName() {
-        Assertions.assertEquals(1,1);
+    @DisplayName("Link output of Object name")
+    void testObjectLink() {
+        Assertions.assertEquals("<a href=\"https://oldschool.runescape.wiki/w/Tree\">Tree</a>",
+                ExaminePanel.getWikiLink("Tree"));
+    }
+
+    @Test
+    @DisplayName("Link output of Objects, NPC's, etc. with spaces in name")
+    void testNameWithSpaces() {
+        Assertions.assertAll(() -> Assertions.assertEquals("<a href=\"https://oldschool.runescape.wiki/w/Adventurer_Jon\">Adventurer Jon</a>",
+                ExaminePanel.getWikiLink("Adventurer Jon")),
+                () -> Assertions.assertEquals("<a href=\"https://oldschool.runescape.wiki/w/Cave_kraken\">Cave kraken</a>",
+                        ExaminePanel.getWikiLink("Cave kraken")),
+                () ->  Assertions.assertEquals("<a href=\"https://oldschool.runescape.wiki/w/Bronze_pickaxe\">Bronze pickaxe</a>",
+                        ExaminePanel.getWikiLink("Bronze pickaxe")),
+                () -> Assertions.assertEquals("<a href=\"https://oldschool.runescape.wiki/w/Robe_bottom_of_darkness\">Robe bottom of darkness</a>",
+                        ExaminePanel.getWikiLink("Robe bottom of darkness"))
+        );
+    }
+
+    @Test
+    @DisplayName("Check for header in Panel")
+    void headerTest(){
+        Assertions.assertTrue(ExaminePanel.heading.isVisible());
     }
 }
